0cb5a9a434fb38d50fdee7cc779c6439e1e53ad1
unknown <marcantoniosmap@gmail.com>
2021-10-20 21:54:47 +0700

    fix preview for news

--- src/Pages/NewsContent.js
+++ src/Pages/NewsContent.js
@@ -1,10 +1,11 @@
 import React, {useEffect,useState} from 'react'
 import Header from '../components/Header';
+import content from "../newscontent";
 
 
 function NewsContent(){
 
-    const recentpost=[1,2,3,4,5]
+    const recentpost=content.slice(0, 4);
     return(
         <>
             <Header 
@@ -14,11 +15,11 @@ function NewsContent(){
                     {name :'home',url:'/'}
                 ]}/>
 
-<div class="container py-4 mb-2">
+<div class="container py-4 mb-4">
 
 <div class="row">
     <div class="col-lg-3 order-lg-2 d-lg-block d-none">
-    <h5 class="font-weight-semi-bold pt-4">Recent Post</h5>
+    <h5 class="font-weight-semi-bold">Recent Post</h5>
         <aside class="sidebar">
             {/* <div class="recent-posts">
 				<article class="post">							
@@ -39,7 +40,7 @@ function NewsContent(){
                                 </div>
                             </div>
                             <div class="post-info">
-                                <a href="blog-post.html">Nullam Vitae Nibh Un Odiosters</a>
+                                <a href="blog-post.html">{post.title}</a>
                                 <div class="post-meta">
                                     Nov 10, 2021
                                 </div>
@@ -58,7 +59,7 @@ function NewsContent(){
             <article class="post post-large blog-single-post border-0 m-0 p-0">
                 <div class="post-image ms-0">
                     <a href="blog-post.html">
-                        <img src={"/img/blog/wide/blog-11.jpg"} class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" />
+                        <img src={"/img/MMS/NewsPreview.jpg"} class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" />
                     </a>
                 </div>
 
@@ -69,19 +70,16 @@ function NewsContent(){
 
                 <div class="post-content ms-0">
 
-                    <h2 class="font-weight-semi-bold"><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
+                    <h2 class="font-weight-semi-bold text-primary">MMS Group Indonesia Distribusikan APD ke Beberapa Rumah Sakit, Puskesmas, Hingga Rumah Yatim dan Yayasan Panti Asuhan</h2>
+                    <div className='pt-3'>
+                   <p>Jakarta 12 Agustus 2020 - MMS Group Indonesia kembali peduli terhadap dampak dari adanya pandemic Covid-19 melalui kegiatan Corporate Social Responbility (CSR).  Sejak April 2020, MMS Group Indonesia telah menyerahkan sumbangan berupa uang tunai, sembako, APD, Masker, Face Shield dan lainnya.</p>
+                    <p>Di bulan Agustus ini sumbangan APD dan juga masker Kembali diserahkan kepada beberapa rumah sakit dan Puskesmas yang menangani pasien covid-19 serta kepada beberapa Rumah Yatim dan Yayasan Panti Asuhan di sekitar Jakarta, Tangerang dan Bogor.</p>
+                    <p>Kami akan terus bekontribusi sebagai bentuk kepedulian kami terhadap kondisi covid-19 ini. Kami sadar betul, ini juga merupakan tanggung jawab kami untuk membantu sesama, bila kemarin kita sudah memberikan sejumlah uang, sembako, masker, face shield, APD dan lainnya, selanjutnya kita akan terus memberikan bantuan yang dibutuhkan, “kata Andrew Hidayat selaku pendiri MMS Group Indonesia, Kamis (12/08).</p>
+                    <p>Saat ini kebutuhan akan APD dan masker bagi tenaga medis masih sangat tinggi, karena itu MMS Group Indonesia berusaha terus menyalurkan bantuan ke berbagai rumah sakit. Bagi para medis sebagai garda terdepan dalam penanganan Covid-19, APD merupakan senjata mutlak untuk membentengi diri agar keamanan dan keselamatannya terjaga.</p>
+                    <p>Bantuan diserahkan langsung oleh tim MMS Group Indonesia peduli Covid-19 kepada perwakilan dari masing-masing Rumah Sakit dan Panti Asuhan.</p>
+                    <p>Di internal Perusahaan, MMS Group Indonesia juga melakukan Rapid Test sampai dengan swab test secara rutin terhadap karyawannya agar dapat mendeteksi sedini mungkin penyebaran Covid-19.</p>
 
-                    <div class="post-meta">
-                        <span><i class="far fa-user"></i> By <a href="#">John Doe</a> </span>
-                        <span><i class="far fa-folder"></i> <a href="#">Lifestyle</a>, <a href="#">Design</a> </span>
-                        <span><i class="far fa-comments"></i> <a href="#">12 Comments</a></span>
                     </div>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lacus, rutrum sit amet placerat et, bibendum nec mauris. Duis molestie, purus eget placerat viverra, nisi odio gravida sapien, congue tincidunt nisl ante nec tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris.</p>
-                    <p>Ut ac elit non mi pharetra dictum nec quis nibh. Pellentesque ut fringilla elit. Aliquam non ipsum id leo eleifend sagittis id a lorem. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam massa mauris, viverra et rhoncus a, feugiat ut sem. Quisque ultricies diam tempus quam molestie vitae sodales dolor sagittis. Praesent commodo sodales purus. Maecenas scelerisque ligula vitae leo adipiscing a facilisis nisl ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-                    <p>Curabitur non erat quam, id volutpat leo. Nullam pretium gravida urna et interdum. Suspendisse in dui tellus. Cras luctus nisl vel risus adipiscing aliquet. Phasellus convallis lorem dui. Quisque hendrerit, lectus ut accumsan gravida, leo tellus porttitor mi, ac mattis eros nunc vel enim. Nulla facilisi. Nam non nulla sed nibh sodales auctor eget non augue. Pellentesque sollicitudin consectetur mauris, eu mattis mi dictum ac. Etiam et sapien eu nisl dapibus fermentum et nec tortor.</p>
-                    <p>Curabitur nec nulla lectus, non hendrerit lorem. Quisque lorem risus, porttitor eget fringilla non, vehicula sed tortor. Proin enim quam, vulputate at lobortis quis, condimentum at justo. Phasellus nec nisi justo. Ut luctus sagittis nulla at dapibus. Aliquam ullamcorper commodo elit, quis ornare eros consectetur a. Curabitur nulla dui, fermentum sed dapibus at, adipiscing eget nisi. Aenean iaculis vehicula imperdiet. Donec suscipit leo sed metus vestibulum pulvinar. Phasellus bibendum magna nec tellus fringilla faucibus. Phasellus mollis scelerisque volutpat. Ut sed molestie turpis. Phasellus ultrices suscipit tellus, ac vehicula ligula condimentum et.</p>
-                    <p>Aenean metus nibh, molestie at consectetur nec, molestie sed nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec euismod urna. Donec gravida pharetra ipsum, non volutpat ipsum sagittis a. Phasellus ut convallis ipsum. Sed nec dui orci, nec hendrerit massa. Curabitur at risus suscipit massa varius accumsan. Proin eu nisi id velit ultrices viverra nec condimentum magna. Ut porta orci quis nulla aliquam at dictum mi viverra. Maecenas ultricies elit in tortor scelerisque facilisis. Mauris vehicula porttitor lacus, vel pretium est semper non. Ut accumsan rhoncus metus non pharetra. Quisque luctus blandit nisi, id tempus tellus pulvinar eu. Nam ornare laoreet mi a molestie. Donec sodales scelerisque congue. </p>
 
                 </div>
             </article>
